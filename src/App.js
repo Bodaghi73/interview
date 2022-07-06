afd5d0d89498d92c938de0eb566e64c0771cb7c9
Bodaghi73 <Bodaghi73>
2022-07-07 01:06:45 +0430

    router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import ArticlesPage from "./pages/articles-page";
 
 function App() {
-  return (
-    <div className="App">
-      <ArticlesPage></ArticlesPage>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/ArticlesPage">
+					<ArticlesPage></ArticlesPage>
+				</Route>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
